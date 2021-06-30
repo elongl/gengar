900ed487e2602b732e671620a4fa12342bb5979a
Elon Gliksberg <elongliks@gmail.com>
2021-06-30 18:01:48 +0300

    Added exit to fatal_error.

--- utils.c
+++ utils.c
@@ -1,10 +1,14 @@
 #include <stdarg.h>
+#include <stdlib.h>
 #include "logger.h"
 
 void fatal_error(char *msg, ...)
 {
     va_list args;
+
     va_start(args, msg);
     log_error(msg, args);
     va_end(args);
+
+    exit(EXIT_FAILURE);
 }
